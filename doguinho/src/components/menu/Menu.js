c97151a025491f6c234fb07ed5fb0606a3963c3f
bruno pessoa <brunopessoa.melo@gmail.com>
2020-07-23 02:09:05 -0300

    adicionando o menu.css e estilizando o componente Menu.

--- doguinho/src/components/menu/Menu.js
+++ doguinho/src/components/menu/Menu.js
@@ -21,11 +21,12 @@ const Menu = () => {
         </div>
 
         <div id={'menu-container'} className="menu-container">
-            <div className="menu-logo"><img className="menu-logo-img" src={logo} alt="logo"/>
+            <div className="menu-logo"><img className="menu-logo-img" src={logo} alt="logo"/></div>
             <div className="menu-title">Caça Raça</div>
             <a className="menu-github" href="https://github.com/brunopeople/Projeto-Doguinho" target="_blank" rel="noopener noreferrer">Link do Repositório</a>
             <a className="menu-twitter" href="https://twitter.com/home" target="_blank" rel="noopener noreferrer">Siga o Twitter</a>
         </div>
+      </div>
     );
 }
 
